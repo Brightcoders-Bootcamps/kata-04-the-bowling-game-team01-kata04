feabf8bbdf69c67196cbda8e9409fe2fdccd56da
FranckZnt19 <frania.sinahi.suarez.lopez@gmail.com>
2021-01-06 17:43:41 -0600

    se modifico el codigo segun los criterios de rubocop quedando de 89 a 18 offenses en los tres archivos de la app

--- src/app.rb
+++ src/app.rb
@@ -1,17 +1,17 @@
-require_relative 'frame.rb'
-require_relative 'score.rb'
+require_relative 'frame'
+require_relative 'score'
 
 current_score = Score.new
 
-for i in (1..10)
-    puts "Frame ##{i}"
-    ball_1 = rand(0..10)
-    if ball_1 == 10
-        ball_2 = 0
-    else
-        ball_2 = rand(0..(10-ball_1))
-    end
-    current_frame = Frame.new(ball_1, ball_2)
-    current_score.add_frame(current_frame)
-    current_score.show_score
+(1..10).each do |i|
+  puts "Frame ##{i}"
+  ball_1 = rand(0..10)
+  ball_2 = if ball_1 == 10
+             0
+           else
+             rand(0..(10 - ball_1))
+           end
+  current_frame = Frame.new(ball_1, ball_2)
+  current_score.add_frame(current_frame)
+  current_score.show_score
 end
